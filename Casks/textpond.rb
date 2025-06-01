5f0622a91bcf2bc99e216cc6c84d7b77b72a0d45
BEW111 <bewilliams111@gmail.com>
2025-06-01 02:28:47 -0400

    fix hash

--- Casks/textpond.rb
+++ Casks/textpond.rb
@@ -1,6 +1,6 @@
 cask "textpond" do
     version "0.1.0-alpha"
-    sha256 "c1042d484032ba3d7bc77b1264dd6c096ae2b8956a2f992168718f7d7a8c195c"
+    sha256 "784a4400c64ea108a2f9418ac8b7f5b4aa481e800a4f9db94f2d053063dafacd"
   
     url "https://github.com/pond-ai/homebrew-textpond/releases/download/v#{version}/Textpond_#{version}_aarch64.dmg",
         verified: "github.com/pond-ai/homebrew-textpond/"
