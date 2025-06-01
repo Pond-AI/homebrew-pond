cc0864bb5940b425fbdd701e30bb01ceeef2df60
BEW111 <bewilliams111@gmail.com>
2025-06-01 01:56:02 -0400

    version fix

--- Casks/textpond.rb
+++ Casks/textpond.rb
@@ -1,8 +1,9 @@
 cask "textpond" do
-    version "0.1.0-alpha"
+    version "0.1.0"
+    version_suffix = "alpha"
     sha256 "c1042d484032ba3d7bc77b1264dd6c096ae2b8956a2f992168718f7d7a8c195c"
   
-    url "https://github.com/pond-ai/homebrew-textpond/releases/download/v#{version}/Textpond-#{version}-aarch64.dmg",
+    url "https://github.com/pond-ai/homebrew-textpond/releases/download/v#{version}-#{version_suffix}/Textpond_#{version}_aarch64.dmg",
         verified: "github.com/pond-ai/homebrew-textpond/"
     name "Textpond"
     desc "Connect your AI apps to your messages"
