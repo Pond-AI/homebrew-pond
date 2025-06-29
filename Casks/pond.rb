9328ba9f2e4cb94013991d1b737a9da5a7c35d5e
BEW111 <bewilliams111@gmail.com>
2025-06-29 09:22:22 -0700

    0.1.6-alpha

--- Casks/pond.rb
+++ Casks/pond.rb
@@ -1,6 +1,6 @@
 cask "pond" do
-  version "0.1.5-alpha"
-  sha256 "8ce56902b6d11208e244d85065b16a9af28ee6eb91878db90fd38d6bbafa954c"
+  version "0.1.6-alpha"
+  sha256 "f2f2eddfadb5a131be9e1512d73273658b881dd1c3f52e804dd3571411375be7"
 
   url "https://github.com/pond-ai/homebrew-pond/releases/download/v#{version}/pond_#{version}_aarch64.dmg",
       verified: "github.com/pond-ai/homebrew-pond/"
